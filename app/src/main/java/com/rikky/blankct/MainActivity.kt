144072c1659d6d99647db8ce46cec0743356530d
Remon Francis <anjalifredy@gmail.com>
2026-09-01 15:33:49 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/rikky/blankct/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.rikky.blankct
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.rikky.blankct.ui.chats.ChatsListFragment
+import com.rikky.blankct.ui.newchat.NewChatFragment
+import com.rikky.blankct.ui.you.YouFragment
+import com.rikky.blankct.ui.yfeed.YFeedFragment
+import com.rikky.blankct.ui.ifeed.IFeedFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            loadFragment(ChatsListFragment())
+        }
+
+        val bottomNav = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setOnItemSelectedListener { item ->
+            val fragment: Fragment = when (item.itemId) {
+                R.id.nav_chats -> ChatsListFragment()
+                R.id.nav_new -> NewChatFragment()
+                R.id.nav_y -> YFeedFragment()
+                R.id.nav_i -> IFeedFragment()
+                R.id.nav_you -> YouFragment()
+                else -> ChatsListFragment()
+            }
+            loadFragment(fragment)
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
