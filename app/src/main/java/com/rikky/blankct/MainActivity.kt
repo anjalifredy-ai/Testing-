0442016f6e6bef88ee47d35f8867fa9345da925c
Remon Francis <anjalifredy@gmail.com>
2026-09-02 16:53:40 +0530

    Update MainActivity.kt

--- app/src/main/java/com/rikky/blankct/MainActivity.kt
+++ app/src/main/java/com/rikky/blankct/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.rikky.blankct
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.ValueEventListener
+import com.rikky.blankct.data.AuthManager
+import com.rikky.blankct.data.CallRepository
 import com.rikky.blankct.ui.chats.ChatsListFragment
 import com.rikky.blankct.ui.newchat.NewChatFragment
 import com.rikky.blankct.ui.you.YouFragment
@@ -10,6 +14,9 @@ import com.rikky.blankct.ui.yfeed.YFeedFragment
 import com.rikky.blankct.ui.ifeed.IFeedFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var incomingCallListener: ValueEventListener? = null
+    private var currentHandledCallId: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +39,35 @@ class MainActivity : AppCompatActivity() {
             loadFragment(fragment)
             true
         }
+
+        AuthManager.ensureSignedIn { _ ->
+            val myUid = AuthManager.currentUid() ?: return@ensureSignedIn
+            incomingCallListener = CallRepository.listenForIncomingCalls(myUid) { call ->
+                if (call.callId != currentHandledCallId) {
+                    currentHandledCallId = call.callId
+                    runOnUiThread {
+                        val intent = Intent(this, CallActivity::class.java)
+                        intent.putExtra("callId", call.callId)
+                        intent.putExtra("otherUid", call.callerId)
+                        intent.putExtra("otherName", call.callerName)
+                        intent.putExtra("isCaller", false)
+                        startActivity(intent)
+                    }
+                }
+            }
+        }
     }
 
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        incomingCallListener?.let {
+            com.google.firebase.database.FirebaseDatabase.getInstance().reference.child("calls").removeEventListener(it)
+        }
     }
 }
